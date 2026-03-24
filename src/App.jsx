bf2cff9ffd36dff18653a4dd1fa6f9d4f824b129
unknown <nlthinh2305@gmail.com>
2026-03-24 15:22:53 +0700

    debug code

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+    <div className="flex h-screen items-center justify-center bg-zinc-900">
+      <h1 className="text-xl font-black text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-600">
+        Tailwind v4 đã chạy! 🚀
+      </h1>
+    </div>
+
+    </div>
+  )
+}
+
+export default App
